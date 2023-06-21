01d2a02387f41b4d2b6dd0bbd4175ec371994658
naveen-krish <42408852+naveen-krish@users.noreply.github.com>
2023-06-21 14:37:49 +0530

    Update langchainbot.py

--- langchainbot.py
+++ langchainbot.py
@@ -16,21 +16,6 @@ from PIL import Image
 
 # Set page title and background image
 st.set_page_config(page_title="DocuBot", page_icon="bot1.jpeg", layout="centered")
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('bg6.jpg') 
 
 # Add image on the right corner
 sidebar_image = "./docs/q&a.png"
