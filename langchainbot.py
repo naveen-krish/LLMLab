e0dddb62d121e317304b594dffccff14960e4fb1
naveen-krish <42408852+naveen-krish@users.noreply.github.com>
2023-06-22 09:41:53 +0530

    Update langchainbot.py

--- langchainbot.py
+++ langchainbot.py
@@ -13,6 +13,23 @@ from io import BytesIO
 import base64
 from PIL import Image
 
+# Set page title and background image
+st.set_page_config(page_title="DocuBot", page_icon="bot1.jpeg", layout="centered")
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('bg5.jpeg')
 
 # Set page title and background image
 st.set_page_config(page_title="DocuBot", page_icon="bot1.jpeg", layout="centered")
@@ -105,7 +122,7 @@ if uploaded_file:
 
 
     def query(q):
-        st.write("Query Response : ", qa.run(q)) 
+        st.write("<p style="color:white;">Query Response : </p>","<p style="color:white;">qa.run(q)"</p>") 
          
     if input_text:
         st.write(query(input_text))
