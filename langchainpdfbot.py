9f73ba2ef3065520ea7bbff311fcc1a1808fbd89
naveen-krish <42408852+naveen-krish@users.noreply.github.com>
2023-07-08 11:56:27 +0530

    Update langchainpdfbot.py

--- langchainpdfbot.py
+++ langchainpdfbot.py
@@ -72,7 +72,7 @@ embeddings = OpenAIEmbeddings()
 
 # Use TextLoader for a single text file or DirectoryLoader for a directory of text files
 
-st.markdown("<h5 style='text-align: center; color: yellow;'>Upload File for Model Analysis</h5>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center; color: yellow;'> PDF Exchange: Upload and Chat! </h5>", unsafe_allow_html=True)
 
 #st.text(" Upload File for Model Analysis ")
 #uploaded_file = st.file_uploader("")
